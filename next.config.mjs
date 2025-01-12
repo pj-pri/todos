e198304f1b9b3513de0d803d876ccbe680d58af4
pj-pri <75967210+pj-pri@users.noreply.github.com>
2025-01-12 18:14:04 +0900

    수정

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	// assetPrefix:
-	// 	process.env.NODE_ENV === 'production'
-	// 		? 'https://깃허브아이디.github.io/레파지토리이름'
-	// 		: '',
-};
-
-export default nextConfig;
